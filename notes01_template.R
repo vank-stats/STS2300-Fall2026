fe7a44e5a2043eb9f985aff3afdb06dffe1f29f5
Ryne VanKrevelen <rvankrevelen@elon.edu>
2026-08-28 12:24:19 -0400

    added template for notes 01 code

---
+++ notes01_template.R
@@ -0,0 +1,37 @@
+# Notes 1 Code Template
+
+
+# Section 2 - Writing R Code
+
+mynumbers <- c(4, 8, 15, 16, 23, 42)
+mynumbers * 2
+max(mynumbers)
+
+my numbers <- c(1, 2, 3)
+
+
+a <- 3
+b <- "frog"
+
+a <- c(1, 4, 2)
+b <- c("frog", "pizza", "elon university")
+
+
+?mtcars
+
+head(mtcars, n = 5)
+
+
+# install.packages(dplyr)
+# install.packages(skimr)
+
+library(dplyr)
+library(skimr)
+
+summary(mtcars)
+View(mtcars)
+glimpse(mtcars)
+skim(mtcars)
+
+
+mtcars$hp
